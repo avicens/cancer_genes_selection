dace113ef9c4d2280890ea721921b9523caf4ace
avicens <chechu086@gmail.com>
2018-06-26 09:40:10 +0200

    loop for all genes created

--- scripts/build_bs_data.R
+++ scripts/build_bs_data.R
@@ -14,13 +14,24 @@ spp$SppCode<-gsub("ENSP00","ENSG00",spp$SppCode)
 #Create dataframe which LRTs will be added to
 bs<-data.frame(Species=spp$fullName)
 
-#2. Load file with node IDs
-ids<-read.table("genes/CCND2/paml/CCND2_node_ids_parsed.txt", sep=";",col.names = "GeneID")
-ids$nodeID<-rownames(ids)
-ids$SppCode<-substr(ids$GeneID,1,6)
+#2. Get significance values of LRTs for each gene
 
-#3. Load LRT file
-lrt<-read.table("genes/CCND2/paml/CCND2_LRT.txt", sep="|", header=TRUE)
+##2.1 Get a list with genenames
+genedir<-"/home/uvi/be/avs/lustre/cancer_genes_selection/genes"
+genelist<-list.files(genedir)
+
+##2.2 Run a loop to extract p-adjusted values of each gene and attach to bs dataframe
+
+for (i in 2) {
+###2.2.1 Load file with node IDs
+  ids<-read.table(file=paste(genedir,"/",genelist[i],"/paml/",genelist[i],"_nodeIDs_parsed.txt", 
+                      sep=""),sep=":",col.names = "GeneID")
+  ids$nodeID<-rownames(ids)
+  ids$SppCode<-substr(ids$GeneID,1,6)
+
+
+###2.2.2. Load LRT file
+lrt<-read.table(file=paste(genedir,"/",genelist[i],"/paml/",genelist[i],"_LRT.txt", sep=""), sep="|", header=TRUE)
 lrt$nodeID<- lrt$Alternativemodel 
 lrt$nodeID %<>% gsub("1$","",.) %>% gsub("[^0-9]+","",.)
 
@@ -32,5 +43,6 @@ merged2$p.adj<-p.adjust(p=merged2$p.value,method = "fdr")
 
 #5. Add LRT data to the branch-site dataframe
 bs<-cbind(bs,merged2$p.adj)
-colnames(bs)[ncol(bs)]=genename
+colnames(bs)[ncol(bs)]<-genelist[i]
+}
 
