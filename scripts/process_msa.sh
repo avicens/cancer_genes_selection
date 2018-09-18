df0ebe9fd8f0bb2db7defcf33ea85b1f6e7515f8
avicens <chechu086@gmail.com>
2018-09-18 15:49:59 +0200

    included step ReplaceStopCodonswithGaps

--- scripts/process_msa.sh
+++ scripts/process_msa.sh
@@ -14,12 +14,12 @@ echo "processing alignment of gene ${gene}"
 ALIGNDIR="${GENEDIR}/${gene}/align"
 
 echo "removing STOP codons"
-perl ReplaceStopWithRefCodonGaps.pl  -pep ${ALIGNDIR}/${gene}_align_AA.fasta -nuc ${ALIGNDIR}/${gene}_align_DNA.fasta -output ${ALIGNDIR}/${gene}_align_DNA_non_stop_codons.fasta 
+perl ReplaceStopCodonswithGaps.pl ${ALIGNDIR}/${gene}_align_DNA.fasta  ${ALIGNDIR}/${gene}_align_DNA_non_stops.fasta 
 
 echo "curating alignment"
-awk '/^>/{f=!d[$1];d[$1]=1}f' ${ALIGNDIR}/${gene}_align_DNA_non_stop_codons.fasta | sed 's/!/N/g' | cut -d '|' -f 1 | awk '/^>/ {printf("%s%s\t",(N>0?"\n":""),$0);N++;next;} {printf("%s",$0);} END {printf("\n");}' | awk '{print substr($0, 1, length($0)-3)}' | tr "\t" "\n" > ${ALIGNDIR}/${gene}_align_DNA_curated.fasta
+awk '/^>/{f=!d[$1];d[$1]=1}f' ${ALIGNDIR}/${gene}_align_DNA_non_stops.fasta | sed 's/!/N/g' | cut -d'|' -f1 | awk '/^>/ {printf("%s%s\t",(N>0?"\n":""),$0);N++;next;} {printf("%s",$0);} END {printf("\n");}' | awk '{print substr($0, 1, length($0)-3)}' | tr "\t" "\n" > ${ALIGNDIR}/${gene}_align_DNA_curated.fasta
 
 echo "trimming alignment for ${gene}"
-trimal -in ${ALIGNDIR}/${gene}_align_DNA_curated.fasta -out ${ALIGNDIR}/${gene}_align_DNA_trimmed2.fasta -gt 0.4 -st 0.1 -resoverlap 0.8 -seqoverlap 70
+trimal -in ${ALIGNDIR}/${gene}_align_DNA_curated.fasta -out ${ALIGNDIR}/${gene}_align_DNA_trimmed.fasta -gt 0.4 -st 0.1 -resoverlap 0.7 -seqoverlap 70
 
 done <<< "${GENENAMES}"
